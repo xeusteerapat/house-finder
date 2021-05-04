e759f6a83b41d4ecafdb66e60db7d64bdb86864d
xeusteerapat <xeus085@gmail.com>
2021-05-04 13:52:11 +0700

    [ADD] add Map component

---
+++ src/components/map.tsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from 'react';
+import Link from 'next/link';
+import { Image } from 'cloudinary-react';
+import ReactMapGL, { Marker, Popup, ViewState } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+interface IProps {}
+
+export default function Map({}: IProps) {
+  const mapRef = useRef<ReactMapGL | null>(null);
+  const [viewport, setViewport] = useState<ViewState>({
+    latitude: 43,
+    longitude: -79,
+    zoom: 10,
+  });
+
+  return (
+    <div className='text-black relative'>
+      <ReactMapGL
+        {...viewport}
+        width='100%'
+        height='calc(100vh - 64px)'
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        ref={instance => (mapRef.current = instance)}
+        minZoom={5}
+        maxZoom={15}
+        mapStyle='mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef'
+      ></ReactMapGL>
+    </div>
+  );
+}
