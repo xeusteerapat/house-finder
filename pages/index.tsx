4c177285ae34dfbab454d36a42dcb8943b365617
xeusteerapat <xeus085@gmail.com>
2021-05-04 13:53:47 +0700

    [ADD] display map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
 import Layout from '../src/components/layout';
+import Map from '../src/components/map';
 
 export default function Home() {
-  return <Layout main={<div>Home</div>} />;
+  return (
+    <Layout
+      main={
+        <div className='flex'>
+          <div
+            className='w-1/2 pb-4'
+            style={{
+              maxHeight: 'calc(100vh - 64px)',
+              overflow: 'scroll',
+            }}
+          >
+            HouseList
+          </div>
+          <div className='w-1/2'>
+            <Map />
+          </div>
+        </div>
+      }
+    />
+  );
 }
